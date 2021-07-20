00555dbac8d9075c017e94a9852c073cdf1ee1d1
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 10:57:38 +0300

    Rokas

--- components/ReadyToStart.js
+++ components/ReadyToStart.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const ReadyToStart = () => {
     return (
-        <div>
-
+        <div className="mt-36  flex flex-col mx-24 max-w-screen-xl flex-grow items-center bg-[#0059F7]">
+            <div>
+                <h1 className="text-[#EBEBED] font-medium text-2xl mt-3 mb-3">READY TO START 🤷‍♂️</h1>
+                <h2 className="text-[#EBEBED] font-medium text-6xl mb-5">Start Your Free Trail Today</h2>
+                <p className="text-[#EBEBED]">Get Started! ▶️</p>
+            </div>
         </div>
     );
 };
