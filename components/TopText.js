4e5f810f92fdfc4bd487913b6b9410ce4ad28067
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 09:34:31 +0300

    Rokas

---
+++ components/TopText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const TopText = () => {
+    return (
+        <div className="flex flex-row mx-20 mt-36 max-w-screen-xl justify-items-center	">
+            <div>
+                <h1 className="text-[#EBEBED] text-7xl font-bold leading-tight max-w-screen-md">The new era of smart emails platform 👨‍💻</h1>
+            </div>
+
+            <div>
+                <div>
+                    <h2 className="text-white text-2xl text-[#7E8BB6] mb-6  mt-5">Here’s why 🚀</h2>
+                </div>
+                <div className="max-w-screen-md">
+                    <h3 className="text-white text-lg mb-8 text-[#7E8BB6] ">Brainlyemails comes packed with templates for any scenario and functionality to let you hit the ground running — in minutes. Meaning, you get to focus on what you do best, while your whole email game is as simple as making a cup of coffee. 🤙</h3>
+                </div>
+                <div className=" flex max-w-xs items-center justify-items-center">
+                    <p className="text-white text-xl p-4 bg-[#0059F7] px-12 rounded-lg">Start Now!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopText;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import TopText from "../components/TopText";
 
 export default function Home() {
   //   h-screen justify-between for making footer
@@ -9,8 +10,11 @@ export default function Home() {
         <title>👋 Brainlyemails</title>
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
+<div className="max-w-screen-3xl">
 
         <Header />
+        <TopText />
+</div>
 
 
       {/*<footer className="flex items-center justify-center w-full h-24 border-t">*/}
