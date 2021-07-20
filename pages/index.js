5653bef3953392b075bc30108e67687fdff13ce1
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 13:26:30 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
 
     <TwoSideCards />
 
-    <div className="flex items-center flex-col mt-32">>
+    <div className="flex items-center flex-col mt-32">
         <h2 className="text-[#7E8BB6] font-medium text-xl mt-3 mb-3">BRAINLYEMAILS</h2>
         <h1 className="text-[#EBEBED] font-medium text-6xl mt-3 mb-12">Pricing</h1>
     </div>
