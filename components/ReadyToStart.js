e78d8cae8444a57dbf406a985bebc5e126155c59
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 11:08:34 +0300

    Rokas

--- components/ReadyToStart.js
+++ components/ReadyToStart.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ReadyToStart = () => {
     return (
-        <div className="mt-36 px-72 py-20 rounded-2xl flex flex-col mx-24 max-w-screen-xl flex-grow bg-[#0059F7]">
+        <div className="mt-36 px-72 py-16 rounded-2xl flex flex-col mx-24 max-w-screen-xl flex-grow bg-[#0059F7]">
             <div className="flex items-center flex-col">
-                <h1 className="text-[#EBEBED] font-medium text-2xl mt-3 mb-3">READY TO START 🤷‍♂️</h1>
-                <h2 className="text-[#EBEBED] font-medium text-6xl mb-5">Start Your Free Trail Today!</h2>
+                <h1 className="text-[#6D9AF3] font-medium text-2xl mt-3 mb-3">READY TO START 🤷‍♂️</h1>
+                <h2 className="text-[#EBEBED] font-medium text-5xl mb-10">Start Your Free Trial Today!</h2>
                 <p className="text-[#EBEBED] px-14 py-4 bg-[#689EFF] rounded-lg font-normal text-xl cursor-pointer hover:bg-[#5886D9] duration-200ms">Get Started! ▶️</p>
             </div>
         </div>
