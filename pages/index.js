dbf9493bd3d9181dad12facd6a4d2b95bf7f7d65
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 11:11:46 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,11 @@ export default function Home() {
     <FiveClicks />
     <ProductCards />
     <ReadyToStart />
+
+    <div className="flex items-center flex-col mt-32">>
+        <h2 className="text-[#7E8BB6] font-medium text-xl mt-3 mb-3">BRAINLYEMAILS</h2>
+        <h1 className="text-[#EBEBED] font-medium text-4xl mt-3 mb-3">How does it work?</h1>
+    </div>
     </div>
     {/*<hr className="divide-y border-light-blue-500"/>*/}
 
