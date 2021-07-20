ccaeb0da850853545eaf40b56d640c4fa1ce74bf
Rokas <rookas.rudzenskas@gmail.com>
2021-07-20 08:32:44 +0300

    Rokas

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="flex items-center justify-between">
+            <div className="flex w-24 h-10">
+                <img src="https://www.brainlyemails.com/assets/img/gallery/logo1.png" alt=""/>
+            </div>
+
+            <div className="flex">
+                <p>Join now!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2">
       <Head>
         <title>👋 Brainlyemails</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
+
+        <Header />
 
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
