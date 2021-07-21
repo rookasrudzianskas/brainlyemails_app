7068e6deca8b6c388ff66cc895d01af1645ed746
Rokas <rookas.rudzenskas@gmail.com>
2021-07-21 09:11:30 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
     <div className=" max-w-full	">>
     <ReadyToStart />
     </div>
-    <div className="flex items-center flex-col mt-32">>
+    <div className="flex items-center flex-col mt-32">
         <h2 className="text-[#7E8BB6] font-medium text-xl mt-3 mb-3">BRAINLYEMAILS</h2>
         <h1 className="text-[#EBEBED] font-medium text-6xl mt-3 mb-3">How does it work?</h1>
     </div>
