d8cf829026fa0d0c65c4396750964a831baa5432
Rokas <rookas.rudzenskas@gmail.com>
2021-07-21 10:03:31 +0300

    Rokas

--- components/TrustedBy.js
+++ components/TrustedBy.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TrustedBy = () => {
     return (
-        <div className="mt-32 grid grid-cols-4 mb-20">
+        <div className="mt-32 grid grid-cols-2 mb-20 gap-10 mr-24 md:grid-cols-4 gap-2">
             {/*<div className="flex">*/}
                 <div className="ml-28">
                     <img src="/images/img_1.png" alt="" className=""/>
@@ -16,7 +16,7 @@ const TrustedBy = () => {
                     <img src="/images/img_3.png" alt="" className=""/>
                 </div>
 
-                <div className="ml-28 ">
+                <div className="ml-28">
                     <img src="/images/img_4.png" alt="" className=""/>
                 </div>
             {/*</div>*/}
