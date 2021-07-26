c519d23a83fb757748c56e4e4a47a1f153a76173
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 18:14:44 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import Footer from "../components/Footer";
 export default function Home() {
   //   h-screen justify-between for making footer
   return (
-    <div className="flex min-w-540 overflow-auto flex-col py-2 bg-[#070E27] ">
+    <div className="flex overflow-auto flex-col py-2 bg-[#070E27] ">
       <Head>
         <title>👋 Brainlyemails</title>
         {/*<link rel="icon" href="/favicon.ico" />*/}
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="flex flex-col md:ml-24">
         <Header />
         </div>
-<   div className="items-center flex flex-col">
+<   div className="items-center flex flex-col mx-0">
         <TopText />
 
         <div className="flex items-center flex-col border-b-2 border-[#1E294E]">
