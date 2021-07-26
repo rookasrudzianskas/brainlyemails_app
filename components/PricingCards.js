5ae90060177723a204c383618224c43a055ac4e3
Rokas <rookas.rudzenskas@gmail.com>
2021-07-26 09:47:21 +0300

    Rokas

--- components/PricingCards.js
+++ components/PricingCards.js
@@ -5,6 +5,7 @@ const PricingCards = () => {
         //sm:flex-row mx-24 max-w-screen-xl justify-items-center mb-32
         //sm:flex-row mx-24 max-w-screen-xl justify-items-center mb-32
         //sm:flex-row mx-24 max-w-screen-xl justify-items-center mb-32
+        //sm:flex-row mx-24 max-w-screen-xl justify-items-center mb-32
         <div className="flex flex-col sm:grid grid-cols-2 gap-4 ">
 
             <div className="mt-24">
